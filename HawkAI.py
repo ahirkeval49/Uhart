99fa5dd8ab7229415f75e09a0e34a5e0744a99c2
Keval Ahir <163200341+ahirkeval49@users.noreply.github.com>
2025-01-17 16:27:44 -0500

    Update HawkAI.py

--- HawkAI.py
+++ HawkAI.py
@@ -7,7 +7,7 @@ from difflib import SequenceMatcher
 # Define function to initialize the Groq model
 def initialize_groq_model():
     return ChatGroq(
-        temperature=none,  # Low temperature to minimize creative generation
+        temperature=0,  # Low temperature to minimize creative generation
         model_name="llama-3.1-8b-instant",
         groq_api_key=st.secrets["general"]["GROQ_API_KEY"]
     )
